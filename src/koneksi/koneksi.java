e9a3525516f59ac9b5a8ddffa154b22ba3220700
User <User@DESKTOP-K7DBVOM>
2026-04-18 21:18:14 +0700

    Tugas Pemograman Visual + form barang

---
+++ src/koneksi/koneksi.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+import java.sql.*;
+/**
+ *
+ * @author User
+ */
+public class koneksi {
+private Connection koneksi;
+    public Connection connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("berhasil konek");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("gagal koneksi" + ex);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/penjualan";
+
+        try {
+            koneksi = DriverManager.getConnection(url, "root", "");
+            System.out.println("berhasil koneksi database");
+        } catch (SQLException ex) {
+            System.out.println("gagal koneksi database" + ex);
+        }
+
+        return koneksi;
+    }
+}
